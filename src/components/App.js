a93091bd3082c53633b41ad18ac7fd15c5a7e815
Natalia-GP <nataliag86@yahoo.es>
2022-12-12 16:34:56 +0100

    template

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+// import { useEffect, useState  } from 'react';
+import '../styles/App.scss';
+// import getDataFromApi from '../services/api'
+// import { Routes, Route, Link, NavLink } from 'react-router-dom';
+
+function App() {
+  // VARIABLES ESTADO
+  // USEEFFECT ?
+  // useEffect (()=>{
+  //   getDataFromApi().then((data)=>{
+  //     console.log(data);
+  //   })
+
+  // }); [])
+  //FUNCIONES ROUTER
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+
+  // HTML EN EL RETURN
+
+  return (
+    <div className="App">
+      <h1 className="title">Rellename!</h1>
+
+      {/* Aquí va tu código HTML. */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+//  const getDataFromApi = () => {
+//     return fetch ('')
+//     .then ((response)=> response.json())
+//     .then ((data)=> {
+
+//     })
+// };
+//  export default getDataFromApi;
